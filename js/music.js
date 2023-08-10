1286e969e9b5e8984994e0cc8b4196e66d6e797f
AmitR1001 <amitrathod98765@gmail.com>
2023-08-10 16:30:22 +0530

    complete

---
+++ js/music.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Hai Junoon",
+        artist: "K.K.",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Adhoore Tum Adhoore Hum",
+        artist: "Vishal Dadlani, Alyssa Mendonsa",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Jaane Kyun",
+        artist: "Vishal–Shekhar",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Jab Mila Tu",
+        artist: "Vishal-Shekhar, Vishal-Shekhar, Vishal Dadlani",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Khabar Nahi",
+        artist: "Vishal-Shekhar,Vishal-Shekhar,Shreya Ghoshal, Amanat Ali, Vishal Dadlani, Raja Hasan",
+    img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Zehnaseeb",
+        artist: "Vishal-Shekhar,Vishal-Shekhar,Chinmayi Sripada,Shekhar Ravjiani",
+    img: "music-6",
+        src: "music-6"
+    },
+];
